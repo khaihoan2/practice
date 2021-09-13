e4744e5698d533b5249fbd23563c1d943e3bb2bd
khaihoan <ngonguyenkhaihoan@gmail.com>
2021-09-13 11:15:13 +0700

    bt

---
+++ src/com/company/main/Main.java
@@ -0,0 +1,10 @@
+package com.company.main;
+
+import com.company.view.MenuContact;
+
+public class Main {
+    public static void main(String[] args) {
+        MenuContact menuContact = new MenuContact();
+        menuContact.run();
+    }
+}
